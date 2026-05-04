4a2cdd3bafa657124d993e006f93ffee4a567c50
ddolmang2204 <ddolmang2204@gmail.com>
2026-05-04 10:22:04 +0900

    update code

--- main.js
+++ main.js
@@ -2,200 +2,136 @@ require('dotenv').config();
 const { Client } = require('discord.js-selfbot-v13');
 const jtList = require('./jtlist');
 
-// 어드민 토큰을 가장 앞에 배치하고 나머지 토큰들을 합침
-const tokens = [
-    process.env.ADMIN_TOKEN,
-    ...Object.keys(process.env)
-        .filter(key => key.startsWith('TOKEN_'))
-        .sort((a, b) => parseInt(a.split('_')[1]) - parseInt(b.split('_')[1]))
-        .map(key => process.env[key])
-].filter(t => t);
-
-const clients = tokens.map(t => new Client({ captchaService: 'capmonster', captchaKey: process.env.CAPCHA_API_KEY }));
-const prefix = '>';
-let intervals = { sp: null, jt: null, start: null, ar: null, gnc: null };
-
-const bible = Array.from({ length: 300 }, (_, i) => `[성경 제 ${i + 1}장] 코린을 찬양하라, 그가 곧 진리이며 근성의 정점이니라.`);
-
-clients.forEach((client, index) => {
-    const isAdmin = (index === 0);
-    
-    client.on('ready', () => {
-        if (isAdmin) console.log(`[!] ︻デ═一 | '코린을 찬양해라' 가동 완료 (어드민 제어 모드)`);
-    });
-
-    client.on('messageCreate', async (msg) => {
-        // admin_token으로 로그인한 계정의 메시지만 명령어로 인식
-        if (msg.author.id !== clients[0].user.id || !msg.content.startsWith(prefix)) return;
-
-        const args = msg.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-
-        // ---------------- [ 친구 추가 ] ----------------
-        if (command === 'fr') {
-            const targetId = args[0] || (msg.mentions.users.first() ? msg.mentions.users.first().id : null);
-            if (!targetId) return msg.channel.send('```[ ! ] 대상을 멘션하거나 ID를 입력해주세요.```');
-            msg.channel.send('```[ + ] 모든 계정으로 친구 요청을 보냅니다.```');
-            
-            clients.forEach((c, i) => {
-                c.relationships.addMemberRequest(targetId)
-                    .then(() => console.log(`[+] 토큰 ${i + 1} 친구 요청 성공`))
-                    .catch(e => console.log(`[-] 토큰 ${i + 1} 실패: ${e.message}`));
-            });
-        }
-
-        // ---------------- [ 서버 조인 ] ----------------
-        else if (command === 'jn') {
-            const inviteCode = args[0];
-            if (!inviteCode) return msg.channel.send('```[ ! ] 초대 코드를 입력해주세요.```');
-            msg.channel.send('```[ + ] 서버 조인을 시작합니다.```');
-            
-            for (const [i, c] of clients.entries()) {
-                try {
-                    await c.acceptInvite(inviteCode);
-                    console.log(`[+] 토큰 ${i + 1} 조인 성공`);
-                    await new Promise(res => setTimeout(res, 2000));
-                } catch (e) {
-                    console.log(`[-] 토큰 ${i + 1} 실패: ${e.message}`);
-                }
-            }
-        }
-
-        // ---------------- [ 스팸 ] ----------------
-        else if (command === 'sp') {
-           msg.channel.send('```\n[ + ] 도배 시작합니다.\n```');
-```');
-            const content = args.join(' ');
-            intervals.sp = setInterval(() => clients.forEach(c => {
-                const ch = c.channels.cache.get(msg.channel.id);
-                if (ch) ch.send('```' + '\n' + content + '\n' + '```');
-            }), 300);
-        } 
-        else if (command === 'sps') {
-            msg.channel.send('```[ + ] 도배 중지합니다.
-```');
-            clearInterval(intervals.sp);
-        }
-        else if (command === 'jt') {
-            msg.channel.send('```[ + ] 장애타 시작합니다.```');
-            intervals.jt = setInterval(() => {
-                const res = jtList[Math.floor(Math.random() * jtList.length)];
-                const count = Math.floor(Math.random() * 473848);
-                clients.forEach(c => {
-                    const ch = c.channels.cache.get(msg.channel.id);
-                    if (ch) ch.send('> ︻デ═一코린에게 개처발린애들수 < ' + count + ' ' + res);
-                });
-            }, 500);
-        } 
-        else if (command === 'jts') {
-            msg.channel.send('```[ + ] 장애타 중지합니다.```');
-            clearInterval(intervals.jt);
-        }
-        else if (command === 'start') {
-            msg.channel.send('```[ + ] 성경 랜덤 읽기 시작합니다.```');
-            let pool = [...bible];
-            intervals.start = setInterval(() => {
-                if (pool.length === 0) pool = [...bible];
-                const line = pool.splice(Math.floor(Math.random() * pool.length), 1)[0];
-                clients.forEach(c => {
-                    const ch = c.channels.cache.get(msg.channel.id);
-                    if (ch) ch.send('```' + line + '```');
-                });
-            }, 1000);
-        } 
-        else if (command === 'stop') {
-            msg.channel.send('```[ + ] 성경 중지합니다.```');
-            clearInterval(intervals.start);
-        }
-
-        // ---------------- [ 유틸 ] ----------------
-        else if (command === 'ct') {
-            const target = msg.mentions.users.first();
-            if (!target) return;
-            let time = 10, done = false;
-            const col = msg.channel.createMessageCollector({ filter: m => m.author.id === target.id, time: 6000 });
-            col.on('collect', () => { done = true; col.stop(); });
-            const t = setInterval(() => {
-                if (done) {
-                    msg.channel.send('```아 아쉽네 거의 다 죽였었는데...```');
-                    return clearInterval(t);
-                }
-                if (time === 0) {
-                    msg.channel.send('```컷컷ㅋㅋㅋㅋㅋ```');
-                    return clearInterval(t);
-                }
-                msg.channel.send(target.toString() + ' ' + (time--));
-            }, 500);
-        }
-        else if (command === 'ar') {
-            msg.channel.send('```[ + ] 오토이모지 시작합니다.```');
-            const target = msg.mentions.users.first();
-            const emoji = args[0];
-            intervals.ar = setInterval(() => {
-                const lastMsg = msg.channel.messages.cache.filter(m => m.author.id === target.id).last();
-                if (lastMsg) lastMsg.react(emoji).catch(() => {});
-            }, 1000);
-        }
-        else if (command === 'ars') {
-            msg.channel.send('```[ + ] 오토이모지 중지합니다.```');
-            clearInterval(intervals.ar);
-        }
-        else if (command === 'cn') {
-            msg.channel.send('```[ + ] 닉네임 변경 시작합니다.```');
-            clients.forEach(c => c.user.setNickname('! 근성의 神 코린').catch(() => {}));
-        }
-        else if (command === 'st') {
-            msg.channel.send('```[ + ] 상태 변경 시작합니다.```');
-            const status = args.join(' ');
-            clients.forEach(c => c.user.setActivity(status));
-        }
-
-        // ---------------- [ 서버 ] ----------------
-        else if (command === 'nuke') {
-            msg.channel.send('```[ + ] 서버 폭파 시작합니다.```');
-            await msg.guild.setName('코린업업이야로');
-            msg.guild.channels.cache.forEach(c => c.delete().catch(() => {}));
-            setInterval(() => {
-                msg.guild.channels.create('코린업', { type: 'GUILD_TEXT' })
-                    .then(c => setInterval(() => c.send('@everyone 코린업'), 300));
-            }, 1000);
-        }
-        else if (command === 'gnc') {
-            msg.channel.send('```[ + ] 그룹이름 변경 시작합니다.
-```');
-            const name = args.join(' ');
-            intervals.gnc = setInterval(() => {
-                if (msg.channel.type === 'GROUP_DM') msg.channel.setName(name);
-            }, 1000);
-        }
-        else if (command === 'gncs') {
-            msg.channel.send('```[ + ] 그룹이름 변경 중지합니다.```');
-            clearInterval(intervals.gnc);
-        }
-
-        // [ 도움말 ]
-        if (command === 'h') {
-            msg.channel.send('```' +
-                '[ ︻デ═一 | \'코린을 찬양해라\' ]\n\n' +
-                '[ 스팸 ]\n' +
-                '>sp [내용]\n' +
-                '>sps\n' +
-                '>jt\n' +
-                '>jts\n' +
-                '>start\n' +
-                '>stop\n\n' +
-                '[ 유틸 ]\n' +
-                '>fr @멘션 | >ct @멘션\n' +
-                '>ar @멘션 [이모지] | >ars\n' +
-                '>cn | >st [상태]\n\n' +
-                '[ 서버 ]\n' +
-                '>jn [초대코드]\n' +
-                '>nuke\n' +
-                '>gnc [이름]\n' +
-                '>gncs' +
-                '```');
-        }
-    });
+const client = new Client({ 
+    captchaService: 'capmonster', 
+    captchaKey: process.env.CAPCHA_API_KEY 
 });
 
-tokens.forEach((t, i) => clients[i].login(t).catch(e => console.log(`[!] 토큰 ${i+1} 로그인 실패`)));
+const prefix = '>';
+let intervals = { sp: null, jt: null, start: null, ar: null, gnc: null };
+const bible = Array.from({ length: 300 }, (_, i) => `[성경 제 ${i + 1}장] 코린을 찬양하라, 그가 곧 진리이며 근성의 정점이니라.`);
+
+client.on('ready', () => {
+    console.log(`[!] ︻デ═一 가동 완료 | 관리자: ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (msg) => {
+    if (msg.author.id !== client.user.id || !msg.content.startsWith(prefix)) return;
+
+    const args = msg.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    // ---------------- [ 전체 명령어 리스트 ] ----------------
+
+    if (command === 'h') {
+        const help = "[ ︻デ═一 | '코린을 찬양해라' ]\n\n" +
+                     ">sp [내용] : 무한 도배 시작\n" +
+                     ">sps : 모든 도배 중지\n" +
+                     ">jt : 장애타 가동\n" +
+                     ">jts : 장애타 중지\n" +
+                     ">start : 성경 랜덤 난사\n" +
+                     ">stop : 성경 난사 중지\n" +
+                     ">fri @멘션 : 즉시 친구 추가\n" +
+                     ">ct [숫자] : 카운트다운 시작\n" +
+                     ">st [상태] : 내 상태 메시지 변경\n" +
+                     ">jn [코드] : 서버 초대 링크로 입장\n" +
+                     ">cls [개수] : 내 메시지 삭제 (청소)\n" +
+                     ">gnc [이름] : 현재 그룹방 이름 무한 변경\n" +
+                     ">gncs : 이름 변경 중지\n" +
+                     ">nuke : (권한있을시) 서버 채널 다 삭제";
+        msg.channel.send("```\n" + help + "\n```").catch(() => {});
+    }
+
+    // 1. 도배 기능
+    else if (command === 'sp') {
+        const content = args.join(' ');
+        if (!content) return;
+        msg.channel.send("```[ + ] 도배를 시작합니다.```");
+        if (intervals.sp) clearInterval(intervals.sp);
+        intervals.sp = setInterval(() => {
+            msg.channel.send(content).catch(() => {});
+        }, 300);
+    }
+    else if (command === 'sps') {
+        clearInterval(intervals.sp);
+        msg.channel.send("```[ + ] 모든 도배 작업을 종료합니다.```");
+    }
+
+    // 2. 장애타 & 성경
+    else if (command === 'jt') {
+        msg.channel.send("```[ + ] 장애타 가동.```");
+        if (intervals.jt) clearInterval(intervals.jt);
+        intervals.jt = setInterval(() => {
+            const res = jtList[Math.floor(Math.random() * jtList.length)];
+            msg.channel.send(`> ︻デ═一 코린 찬양 | ${res}`).catch(() => {});
+        }, 500);
+    }
+    else if (command === 'jts') {
+        clearInterval(intervals.jt);
+        msg.channel.send("```[ + ] 장애타를 중지합니다.```");
+    }
+    else if (command === 'start') {
+        msg.channel.send("```[ + ] 성경 난사를 시작합니다.```");
+        if (intervals.start) clearInterval(intervals.start);
+        intervals.start = setInterval(() => {
+            const line = bible[Math.floor(Math.random() * bible.length)];
+            msg.channel.send(line).catch(() => {});
+        }, 1000);
+    }
+    else if (command === 'stop') {
+        clearInterval(intervals.start);
+        msg.channel.send("```[ + ] 모든 성경 작업을 중지합니다.```");
+    }
+
+    // 3. 관리 & 유틸
+    else if (command === 'fri') {
+        const targetId = args[0] || (msg.mentions.users.first() ? msg.mentions.users.first().id : null);
+        if (!targetId) return;
+        client.user.addFriend(targetId).then(() => msg.channel.send("```[ + ] 친추 완료.```")).catch(() => {});
+    }
+    else if (command === 'ct') {
+        let num = parseInt(args[0]);
+        if (isNaN(num)) return;
+        const timer = setInterval(() => {
+            if (num <= 0) {
+                msg.channel.send("```[ ! ] 종료.```");
+                clearInterval(timer);
+            } else {
+                msg.channel.send(`\`\`\`${num--}\`\`\``);
+            }
+        }, 1000);
+    }
+    else if (command === 'st') {
+        const text = args.join(' ');
+        client.user.setActivity(text);
+        msg.channel.send(`\`\`\`[ + ] 상태 변경: ${text}\`\`\``);
+    }
+    else if (command === 'jn') {
+        const code = args[0];
+        client.fetchInvite(code).then(inv => inv.acceptInvite(true)).then(() => msg.channel.send("```[ + ] 서버 입장 완료.```")).catch(() => {});
+    }
+    else if (command === 'cls') {
+        const num = parseInt(args[0]) || 10;
+        msg.channel.messages.fetch({ limit: 100 }).then(msgs => {
+            const myMsgs = msgs.filter(m => m.author.id === client.user.id).first(num);
+            myMsgs.forEach(m => m.delete().catch(() => {}));
+        });
+    }
+
+    // 4. 그룹방 이름 테러
+    else if (command === 'gnc') {
+        const name = args.join(' ');
+        if (msg.channel.type !== 'GROUP_DM') return;
+        msg.channel.send("```[ + ] 그룹명 변경 테러 시작.```");
+        if (intervals.gnc) clearInterval(intervals.gnc);
+        intervals.gnc = setInterval(() => {
+            msg.channel.setName(name + " " + Math.random().toString(36).substring(2, 5)).catch(() => {});
+        }, 1000);
+    }
+    else if (command === 'gncs') {
+        clearInterval(intervals.gnc);
+        msg.channel.send("```[ + ] 그룹명 변경을 중지합니다.```");
+    }
+});
+
+client.login(process.env.ADMIN_TOKEN).catch(() => console.log("토큰 에러. .env 확인해라."));
